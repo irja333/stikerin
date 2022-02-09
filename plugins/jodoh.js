d974f07631550dc3f47e0601476762d2379824dc
IRJA <93579297+irja333@users.noreply.github.com>
2022-02-09 23:51:19 +0700

    Update jodoh.js

--- plugins/jodoh.js
+++ plugins/jodoh.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch')
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) throw `contoh:\n${usedPrefix + command} ariffb|amel`
     let [nama1, nama2] = text.split(/[&|.]/i)
-    if (!nama1 || !nama2) throw `contoh:\n${usedPrefix + command} ariffb|amel`
+    if (!nama1 || !nama2) throw `contoh:\n${usedPrefix + command} irja|ara`
 
     let res = await fetch(global.API('zeks', '/api/primbonjodoh', { nama1, nama2 }, 'apikey'))
     if (!res.ok) throw eror
